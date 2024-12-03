3da5f05ddac0afdd5c7c2da58d74deb99af234bb
DayanAguilar <dayan.aguilar.alcocer@gmail.com>
2024-12-03 11:37:35 -0400

    Añadimos el caso para cuando es 3 devuelve Fizz

--- src/sumador.js
+++ src/sumador.js
@@ -2,6 +2,7 @@ function sumar(a, b) {
   return a + b;
 }
 function generarFizzBuzzPara(n){
+  if(n===3){return "Fizz"}
   return "FizzBuzz";
 }
 export default generarFizzBuzzPara;
--- src/sumador.test.js
+++ src/sumador.test.js
@@ -5,6 +5,9 @@ describe("FizzBuzz", () => {
   it("deberia devolver fizz buzz porque es 0", () => {
     expect(generarFizzBuzzPara(0)).toEqual("FizzBuzz");
   });
+  it("deberia devolver fizz porque es 3", () => {
+    expect(generarFizzBuzzPara(3)).toEqual("Fizz");
+  });
 });
 
 
